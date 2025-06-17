65ad1e88b9c9ae298bf3eda1b53e6ce4fc32033b
suyash pandey <suyashpandey@suyashs-MacBook-Air.local>
2025-06-18 01:07:22 +0530

    Your message describing the changes

--- q27.py
+++ q27.py
@@ -1,14 +1,14 @@
 # Create a custom module named calculator.py with add, subtract, multiply, and divide functions.
 # Import and use it in another script.
 
-from calculator import addition,substract,multiply,divide
+from calculator import add,substraction,multiply,divide
 
-num_1=int(input("Enter a number"))
-num_2=int(input("Enter another number"))
+num_1=int(input("Enter a number :"))
+num_2=int(input("Enter another number :"))
 
-addition(num_1,num_2)
-substract(num_1,num_2)
-multiply(num_1,num_2)
-divide(num_1,num_2)
+print(add.addition(num_1,num_2))
+print(substraction.substract(num_1,num_2))
+print(multiply.multiply(num_1,num_2))
+print(divide.divide(num_1,num_2))
 
 
